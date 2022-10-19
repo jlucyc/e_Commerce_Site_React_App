d4b41c9f9cbd4feaa6b7e026a39fb5663c7fc95e
Louise McCafferty <mccafferty_@hotmail.com>
2022-10-19 14:32:22 +0100

    finished two of thre mvp tasks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Basket from './components/Basket';
+import NavBar from "./components/NavBar"
 import FilmContainer from './containers/FilmContainer';
 import './App.css';
 
+
+
 function App() {
   return (
+    <Router>
     <>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<FilmContainer/>}/>
+      <Route path="/basket" element={<Basket/>}/>
+    </Routes>
+
     <div className="App">
     <FilmContainer/>
     </div>
     </>
+    </Router>
   );
 }
 
